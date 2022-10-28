e9b6e7cdde063b0eecb37f0d059b53203913564a
Woray <Warisreal@hotmail.co.uk>
2022-10-28 17:31:14 +0100

    change2

--- app.js
+++ app.js
@@ -2,10 +2,8 @@
 
 const key = document.querySelector(".button")
 const txtfield = document.querySelector("#textfield")
-//const clear = document.querySelector("#clear")
-const container = document.querySelector("#con")
-const operator = document.querySelector("#operator")
-const equals = document.querySelector("#equals")
+const clear = document.querySelector("#clear")
+//const container = document.querySelector("#con")
 
 //Handles Num event click
 function numHandler(event) {
@@ -15,26 +13,6 @@ function numHandler(event) {
   txtfield.appendChild(numClick)
 }
 let numClick = key.addEventListener("click", numHandler)
-
-//Handles operator click event
-function operHandler(event) {
-  let operatorCapture = event.target.innerText
-  let operCLick = document.createTextNode(operatorCapture)
-  txtfield.appendChild(operCLick)
-}
-operator.addEventListener("click", operHandler)
-
-//Handles equals click event
-function equalsHandler(event) {
-  let equalCapture = event.target.innerText
-  if (event.target.innerText === "=") {
-    let equalCapture = num + num
-  }
-  //Handles display
-  let equalCLick = document.createTextNode(equalCapture)
-  txtfield.appendChild(equalCLick)
-}
-equals.addEventListener("click", equalsHandler)
 
 function clearHandle() {
   txtfield.textContent = ""
